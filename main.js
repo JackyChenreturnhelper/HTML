6024559aebf8bbdf33993f8cebbeda35d3b20cbb
jacky.chen <jacky.chen@returnhelper.com>
2020-08-31 18:38:15 +0800

    RWD

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-var indeximg = document.getElementById("indeximg");
 
-indeximg.addEventListener("click", function () {
-    document.getElementById("indeximg").classList.add("display-no");
-    document.getElementById("indexbody").classList.remove("display-no");
-});
+var isMobile = /Android|webOS|iPhone|iPad|BlackBerry/i.test(navigator.userAgent)
+
+}
